5ca9d909bf0da6202d8fb99338afc62e5a93f441
psc-hub <pschahal@gmail.com>
2026-05-22 05:59:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='fl-v1';
+const ASSETS=['./FreightLedger.html','./manifest.json','./icon-512.svg','./icon-192.svg','./icon-maskable.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./FreightLedger.html'))));});
